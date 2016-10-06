97c81ee7e6cfe96fdc2ed328a51c7d5a129b201a
MathieuM1 <mat.mercier45@gmail.com>
2016-10-06 20:54:11 +0200

    ajout video en background, mise en forme index.css

--- index.php
+++ index.php
@@ -3,13 +3,36 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>WeatherMood</title>
-		<link rel="stylesheet" href="custom.css">
+
+		<!-- CSS BOOTSTRAP -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- CSS PERSO-->
+		<link rel="stylesheet" href="css/index.css">
+
 	</head>
 	<body>
-		<form method="POST" action="page.php">
-			<label for="search">WeatherMood Search!</label><br/>
-			<input type="text" name="search" id="search" placeholder="Entrez une ville"></input>
-			<input type="submit" name="submit" id="submit" value="GO!"></input><br/>
-		</form>
+	<video class="bgvid" playsinline autoplay muted loop>
+		<!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+		<source src="image/sky.mp4" type="video/mp4">
+	</video>
+		<div class="container-fluid formulaire">
+			<div class="row">
+				<div class="col-xs-12 ">
+					<form method="POST" action="page.php">
+						<label class="titleSearch" for="search">WeatherMood Search!</label><br/>
+						<input class="form-control" type="text" name="search" id="search" placeholder="Entrez une ville"></input>
+						<input type="submit" class="btn btn-primary" name="submit" id="submit" value="GO!"></input><br/>
+					</form>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
--- page.php
+++ page.php
@@ -1,4 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8">
+		<title>WeatherMood</title>
+
+		<!-- CSS BOOTSTRAP -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- CSS PERSO-->
+		<link rel="stylesheet" href="page.css">
+</head>
+<body>
+	<?php
 	if (isset($_POST['submit'])){
 		if (empty($_POST['search'])){
 			echo '<br/> Vous n\'avez pas entré le nom d\'une ville dans le champ';
@@ -45,3 +65,7 @@
 			$idCloud[600 to 655]=>'playlistNuage');*/
 	}
 ?>
+</body>
+</html>
+
+
